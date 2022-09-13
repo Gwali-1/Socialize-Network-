11e7271cacafcdd32c83d6384c5797eb4bede9ef
Gwali <gwalisam37@gmail.com>
2022-09-13 16:16:57 +0000

    model migrations

--- network/admin.py
+++ network/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import User
+from .models import User,Post,Followers,Following
 
 
 # Register your models here.
 
 
 admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Followers)
+admin.site.register(Following)
--- network/models.py
+++ network/models.py
@@ -1,25 +1,30 @@
-from operator import mod
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followers_number = models.IntegerField(default=0)
+    following_number = models.IntegerField(default=0)
 
 
 
 class Post(models.Model):
-    content = models.Char
+    content = models.CharField(max_length=200)
+    likes = models.IntegerField(default=0)
+    created = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="post_created")
+
+
 
 class Followers(models.Model):
-    followers = models.ForeignKey(User,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    followers_number = models.IntegerField(default=0)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="user_followers")
+    follower = models.ForeignKey(User,on_delete=models.CASCADE)
+   
 
 class Following(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="user_following")
     following = models.ForeignKey(User,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    following_number = models.IntegerField(default=0)
+
 
 
 
