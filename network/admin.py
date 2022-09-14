780274746ce96fd405ebcc6a4365e64689987139
Gwali <gwalisam37@gmail.com>
2022-09-14 03:00:21 +0000

    added display formatting settings to admin.py file

--- network/admin.py
+++ network/admin.py
@@ -4,8 +4,20 @@ from .models import User,Post,Followers,Following
 
 # Register your models here.
 
+class Useradmin(admin.ModelAdmin):
+    list_display = ("id","first_name","username","followers_number","following_number")
 
-admin.site.register(User)
-admin.site.register(Post)
-admin.site.register(Followers)
-admin.site.register(Following)
+
+class Postadmin(admin.ModelAdmin):
+    list_display = ("id","user","created","likes")
+
+class Followersadmin(admin.ModelAdmin):
+    list_display = ("id","user","follower")
+
+class Followingadmin(admin.ModelAdmin):
+    list_display = ("id","user","following")
+
+admin.site.register(User,Useradmin)
+admin.site.register(Post,Postadmin)
+admin.site.register(Followers,Followersadmin)
+admin.site.register(Following,Followingadmin)
